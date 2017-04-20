aa7fe4b6e8546749dc2a00928e4fc028ec13fc2c
Jatin Nanda <jatin.nanda1337@gmail.com>
2017-04-20 01:19:22 -0400

    baseline complete I think

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import copy
 import time
 from utils import *
 
@@ -13,22 +14,9 @@ mode = sys.argv[3]
 
 
 #parse input
-num_routers, routers, edges = input_to_routers(file_routers)
+num_routers, routers = input_to_routers(file_routers)
 changes = input_to_changes(file_changes, num_routers)
 changes.sort(key = lambda c: c.time_step)
-
-#print statement examples
-print("NUM ROUTERS:" + str(num_routers))
-print("ROUTERS:")
-for r in routers:
-    print(r)
-    print(r.adjacencies)
-print("EDGES:")
-for e in edges:
-    print(e)
-print("CHANGES:")
-for c in changes:
-    print(c)
 
 #output files
 basic = open('basic.txt', 'w')
@@ -38,10 +26,17 @@ poison_reverse = open('poison.txt', 'w')
 #main loop
 iter_num = 0
 converged = False
-while (not converged):
-    if max(edges, key = lambda e : e.cost).cost >= 100:
-        print("Count to infinity instability detected. Stopping iterations.")
-        break
+
+
+print("INITIAL TABLE")
+for r in routers:
+    print(r.print_table())
+
+while (not converged or len(changes) > 0):
+    for r in routers:
+        if int(max(r.adjacencies, key = lambda adj: adj[1])[1]) >= 100:
+            print("Count to infinity instability detected. Stopping iterations.")
+            break
 
     print("ITERATION " + str(iter_num))
     time.sleep(1) #delay for readability
@@ -49,11 +44,37 @@ while (not converged):
     #apply changes when applicable
     if len(changes) > 0 and iter_num == int(changes[0].time_step):
         print("Applying change: " + str(changes[0]))
-        changes[0].apply_change(routers, edges)
+        changes[0].apply_change(routers)
         del changes[0]
+        converged = False
 
-    #code for basic protocol
+    #initialize tables with adjacencies
+    for router in routers:
+        for adj in router.adjacencies:
+            router.table[adj[0].name] = Advertisement(adj[0].name, adj[1])
 
+    #temp copy of every table in routers
+    temp_tables = {r.name: copy.copy(r.table) for r in routers}
+    for router in routers:
+        for adj in router.adjacencies:
+            table_to_use = temp_tables[adj[0].name]
+            for advert in table_to_use:
+                if advert in router.table:
+                    new_cost = router.table[adj[0].name].cost + table_to_use[advert].cost
+                    if new_cost < router.table[advert].cost:
+                        router.table[advert].cost = new_cost
+                        router.table[advert].next_hop = adj[0].name
+
+
+    print("UPDATED TABLE")
+    for r in routers:
+        print(r.print_table())
+
+    #check if no updates were made
+    tables = {r.name: r.table for r in routers}
+    if tables == temp_tables:
+        print("CONVERGED")
+        converged = True
 
     #code for split horizon
 
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 def input_to_routers(filename):
     num_routers = None
     routers = []
-    edges = []
     with open(filename) as f:
         inputs = f.readlines()
         for i, line in enumerate(inputs):
@@ -12,22 +11,21 @@ def input_to_routers(filename):
                 #parse router ids
                 router_a = Router(line[0], num_routers)
                 router_b = Router(line[1], num_routers)
+                edge_cost = int(line[2])
                 #add adjacencies
                 if router_a in routers:
-                    routers[routers.index(router_a)].adjacencies.append(router_b)
+                    routers[routers.index(router_a)].adjacencies.append((router_b, edge_cost))
                 else:
-                    router_a.adjacencies.append(router_b)
+                    router_a.adjacencies.append((router_b, edge_cost))
                     routers.append(router_a)
 
                 if router_b in routers:
-                    routers[routers.index(router_b)].adjacencies.append(router_a)
+                    routers[routers.index(router_b)].adjacencies.append((router_a, edge_cost))
                 else:
-                    router_b.adjacencies.append(router_a)
+                    router_b.adjacencies.append((router_a, edge_cost))
                     routers.append(router_b)
 
-                #make edge
-                edges.append(Edge(router_a, router_b, line[2]))
-    return (num_routers, routers, edges)
+    return (num_routers, routers)
 
 
 def input_to_changes(filename, num_routers):
@@ -36,18 +34,31 @@ def input_to_changes(filename, num_routers):
         inputs = f.readlines()
         for line in inputs:
             line = line.split(' ')
-            time_step = line[0]
+            time_step = int(line[0])
             router_a = Router(line[1], num_routers)
             router_b = Router(line[2], num_routers)
-            edge = Edge(router_a, router_b, line[3])
-            changes.append(Change(time_step, edge))
+            cost = int(line[3])
+            changes.append(Change(time_step, router_a, router_b, cost))
     return changes
 
 class Router():
     def __init__(self, name, total_routers):
         self.name = name
         self.adjacencies = []
-        self.table = {}
+        self.table = self.init_table(total_routers)
+
+    def init_table(self, total_routers):
+        table = {}
+        for i in range(total_routers):
+            if i+1 is not int(self.name):
+                table[str(i+1)] = Advertisement(None, float('inf'))
+        return table
+
+    def print_table(self):
+        table = ''
+        for entry in self.table:
+            table += str(self.name) + "->" + str(entry) + " " + (str(self.table[entry]) + "\n")
+        return table
 
     def print_adjacencies(self):
         return str(self.adjacencies)
@@ -58,34 +69,58 @@ class Router():
     def __eq__(self, other):
         return self.name == other.name
 
-class Edge():
-    def __init__(self, router_a, router_b, cost):
-        self.router_a = router_a
-        self.router_b = router_b
-        self.cost = int(cost)
+    def __hash__(self):
+        return int(self.name)
 
-    def __repr__(self):
-        return str(self.router_a) + "->" + str(self.router_b) + " COST: " + str(self.cost)
-
-    def __eq__(self, other):
-        return (self.router_a == other.router_a) and (self.router_b == other.router_b)
 
 class Change():
-    def __init__(self, time_step, edge):
+    def __init__(self, time_step, router_a, router_b, cost):
         self.time_step = time_step
-        self.edge = edge
+        self.router_a = router_a
+        self.router_b = router_b
+        self.cost = cost
 
-    def apply_change(self, routers, edges):
-        if self.edge in edges:
-            ind = edges.index(self.edge)
-            edges[ind].cost = self.edge.cost
-            #delete edge
-            if (self.edge.cost) == -1:
-                del edges[ind]
+    def apply_change(self, routers):
+        #update routers and adjacencies
+        router_a = [r for r in routers if r == self.router_a][0]
+        router_b = [r for r in routers if r == self.router_b][0]
+
+        #deleting edge, get rid of adjacencies
+        if int(self.cost) == -1:
+            print("DELETING EDGE")
+            b_cost = dict(router_a.adjacencies)[router_b]
+            del router_a.adjacencies[router_a.adjacencies.index((router_b, b_cost))]
+            a_cost = dict(router_b.adjacencies)[router_a]
+            del router_b.adjacencies[router_b.adjacencies.index((router_a, a_cost))]
+            self.cost = float('inf')
+        #new edge
+        elif self.router_b not in dict(router_a.adjacencies):
+            print("ADDING NEW EDGE")
+            router_a.adjacencies.append((router_b, self.cost))
+            router_b.adjacencies.append((router_a, self.cost))
+        #existing edge
         else:
-            edges.append(self.edge)
-        return edges
+            del router_a.adjacencies[router_a.adjacencies.index(router_b, self.cost)]
+            del router_b.adjacencies[router_b.adjacencies.index(router_a, self.cost)]
+            router_a.adjacencies.append((router_b, self.cost))
+            router_b.adjacencies.append((router_a, self.cost))
+
+        #update in the routing tables
+        if router_a.table[router_b.name].cost > self.cost:
+            router_a.table[router_b.name].cost = self.cost
+            router_a.table[router_b.name].next_hop = self.router_b
+
+        if router_b.table[router_a.name].cost > self.cost:
+            router_b.table[router_a.name].cost = self.cost
+            router_b.table[router_a.name].cost = self.router_a
 
     def __repr__(self):
-        return "TIME: " + str(self.time_step) + " EDGE:" + str(self.edge)
+        return "TIME: " + str(self.time_step) + " EDGE:" + str(self.router_a) + "->" + str(self.router_b) + " COST: " + str(self.cost)
 
+class Advertisement():
+    def __init__(self, next_hop, cost):
+        self.next_hop = next_hop
+        self.cost = cost
+
+    def __repr__(self):
+        return str(self.next_hop) + ": " + str(self.cost)
